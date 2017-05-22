f4f57563750eb782f79b2feff745c34739c54560
Stephen Hibbs <steve@alpinelaboratories.com>
2017-05-22 14:43:40 -0600

    made commit for ble-scanner branch that tries to scan for test-04

--- pulse-script.sh
+++ pulse-script.sh
@@ -1,2 +1,2 @@
-#sudo python alpine_utilities/pulse-BLE-validation.py $TESTID
-sudo /home/pi/alpine_utilities/pulse-flashloader
+sudo python alpine_utilities/pulse-BLE-validation.py 04
+# sudo /home/pi/alpine_utilities/pulse-flashloader
